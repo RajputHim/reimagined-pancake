a699ba741b1f7bdd59329e9ea3017b7ae5a6cc54
RajputHim <105914405+RajputHim@users.noreply.github.com>
2023-02-10 23:09:41 +0530

    Added find by category method

--- GorgeousGlamApplication/src/main/java/com/GorgeousGlam/repository/ProductRepo.java
+++ GorgeousGlamApplication/src/main/java/com/GorgeousGlam/repository/ProductRepo.java
@@ -1,5 +1,7 @@
 package com.GorgeousGlam.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.GorgeousGlam.model.Product;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+	public List<Product> findByCategory(String category);
 
 }
