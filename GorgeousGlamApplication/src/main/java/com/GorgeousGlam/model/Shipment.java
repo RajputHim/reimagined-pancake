6bfbd170025365a10490fbb8b35d748f8c1abe03
RajputHim <105914405+RajputHim@users.noreply.github.com>
2023-02-14 22:41:33 +0530

    Added validation on shipment

--- GorgeousGlamApplication/src/main/java/com/GorgeousGlam/model/Shipment.java
+++ GorgeousGlamApplication/src/main/java/com/GorgeousGlam/model/Shipment.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Shipment {
@@ -15,8 +16,10 @@ public class Shipment {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int shipmentId;
 
+	@NotNull(message = "DeliveryFrom cannot be null")
 	private String deliveryFrom;
 
+	@NotNull
 	private LocalDate expectedDelivery;
 
 	@OneToOne
