5cab5c7ca0db3ba3e5ea439e155a06eebd4a48da
RajputHim <105914405+RajputHim@users.noreply.github.com>
2023-02-14 22:35:01 +0530

    Added shipment exception handler

---
+++ GorgeousGlamApplication/src/main/java/com/GorgeousGlam/exception/ShipmentException.java
@@ -0,0 +1,15 @@
+package com.GorgeousGlam.exception;
+
+public class ShipmentException extends RuntimeException{
+	
+	public ShipmentException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public ShipmentException(String message) {
+		super(message);
+	}
+	
+	
+}
