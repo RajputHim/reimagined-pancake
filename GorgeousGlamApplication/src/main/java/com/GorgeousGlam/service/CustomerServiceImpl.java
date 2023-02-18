89cdebc1fa4c6d81d5375100f50ec42704c54269
RajputHim <105914405+RajputHim@users.noreply.github.com>
2023-02-18 21:40:06 +0530

    Added delete address method and Add address method

--- GorgeousGlamApplication/src/main/java/com/GorgeousGlam/service/CustomerServiceImpl.java
+++ GorgeousGlamApplication/src/main/java/com/GorgeousGlam/service/CustomerServiceImpl.java
@@ -2,10 +2,14 @@ package com.GorgeousGlam.service;
 
 import java.util.List;
 
+import javax.security.auth.login.LoginException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.GorgeousGlam.DTO.AddressDTO;
 import com.GorgeousGlam.DTO.CustomerDTO;
+import com.GorgeousGlam.exception.AddressException;
 import com.GorgeousGlam.exception.CustomerException;
 import com.GorgeousGlam.model.Address;
 import com.GorgeousGlam.model.Customer;
@@ -21,6 +25,9 @@ public class CustomerServiceImpl implements ICustomerService {
 	@Autowired
 	private AddressRepo addressRepo;
 
+	@Autowired
+	private ILoginService loginService;
+
 	@Override
 	public Customer addCustomer(CustomerDTO customer) throws CustomerException {
 
@@ -32,6 +39,8 @@ public class CustomerServiceImpl implements ICustomerService {
 
 		Address customerAddress = new Address(customer.getAddress().getHouseNo(), customer.getAddress().getColony(),
 				customer.getAddress().getCity(), customer.getAddress().getState(), customer.getAddress().getPinCode());
+
+		customerAddress.setCustomer(newCustomer);
 
 		newCustomer.getAddress().add(customerAddress);
 
@@ -48,60 +57,45 @@ public class CustomerServiceImpl implements ICustomerService {
 	}
 
 	@Override
-	public Customer deleteCustomerById(Integer customerId) throws CustomerException {
+	public Customer deleteCustomerById(Integer customerId) throws CustomerException, LoginException {
 		Customer customer = customerRepo.findById(customerId)
 				.orElseThrow(() -> new CustomerException("No customer found by id: " + customerId));
+
+		loginService.logout(customerId);
 
 		customerRepo.delete(customer);
 		return customer;
 	}
 
 	@Override
-	public Customer updateCustomerDetails(Customer customer, Integer addressId) throws CustomerException {
-		Customer existingCustomer = customerRepo.findById(customer.getCustomerId())
-				.orElseThrow(() -> new CustomerException("No customer found by id: " + customer.getCustomerId()));
+	public Customer updateCustomerDetails(CustomerDTO customer, Integer addressId, Integer customerId)
+			throws CustomerException {
+		Customer existingCustomer = customerRepo.findById(customerId)
+				.orElseThrow(() -> new CustomerException("No customer found by id: " + customerId));
 
-		if (customer.getEmail() != null) {
-			existingCustomer.setEmail(customer.getEmail());
+		existingCustomer.setEmail(customer.getEmail());
+
+		existingCustomer.setName(customer.getName());
+
+		existingCustomer.setPassword(customer.getPassword());
+
+		AddressDTO newAddress = customer.getAddress();
+
+		List<Address> allAddresses = existingCustomer.getAddress();
+
+		for (Address address : allAddresses) {
+			if (address.getAddressId() == addressId) {
+				address.setCity(newAddress.getCity());
+				address.setColony(newAddress.getColony());
+				address.setHouseNo(newAddress.getHouseNo());
+				address.setPinCode(newAddress.getPinCode());
+				address.setState(newAddress.getState());
+				return customerRepo.save(existingCustomer);
+			}
+
 		}
 
-		if (customer.getName() != null) {
-			existingCustomer.setName(customer.getName());
-		}
-
-		if (customer.getPassword() != null) {
-			existingCustomer.setPassword(customer.getPassword());
-		}
-
-//		addressRepo.findById(addressId).orElseThrow();
-//		
-//		List<Address> newAddress = customer.getAddress();
-//		if (customer.getAddress() != null) {
-//			Address oldAddress = existingCustomer.getAddress();
-//
-//			if (newAddress.getCity() != null) {
-//				oldAddress.setCity(newAddress.getCity());
-//			}
-//
-//			if (newAddress.getColony() != null) {
-//				oldAddress.setColony(newAddress.getColony());
-//			}
-//
-//			if (newAddress.getHouseNo() != null) {
-//				oldAddress.setHouseNo(newAddress.getHouseNo());
-//			}
-//
-//			if (newAddress.getState() != null) {
-//				oldAddress.setState(newAddress.getState());
-//			}
-//
-//			if (newAddress.getPinCode() != null) {
-//				oldAddress.setPinCode(newAddress.getPinCode());
-//			}
-
-//		}
-
-		return customerRepo.save(existingCustomer);
+		throw new AddressException("Address id is wrong..");
 
 	}
 
@@ -114,6 +108,40 @@ public class CustomerServiceImpl implements ICustomerService {
 		}
 
 		return allCustomers;
+	}
+
+	@Override
+	public Customer addAddress(Address address, Integer customerId) throws AddressException {
+
+		Customer customer = customerRepo.findById(customerId)
+				.orElseThrow(() -> new CustomerException("No customer found by id: " + customerId));
+
+		address.setCustomer(customer);
+		customer.getAddress().add(address);
+
+		customerRepo.save(customer);
+
+		return customer;
+	}
+
+	@Override
+	public Customer deleteAddress(Integer addressId, Integer customerId) throws AddressException {
+		Customer customer = customerRepo.findById(customerId)
+				.orElseThrow(() -> new CustomerException("No customer found by id: " + customerId));
+
+		List<Address> allAddresses = customer.getAddress();
+
+		for (Address address : allAddresses) {
+			if (address.getAddressId() == addressId) {
+				allAddresses.remove(address);
+				customer.setAddress(allAddresses);
+				addressRepo.deleteById(addressId);
+				return customer;
+			}
+		}
+
+		throw new AddressException("No address found by id: " + addressId);
+
 	}
 
 }
--- GorgeousGlamApplication/src/main/java/com/GorgeousGlam/service/ICustomerService.java
+++ GorgeousGlamApplication/src/main/java/com/GorgeousGlam/service/ICustomerService.java
@@ -2,8 +2,12 @@ package com.GorgeousGlam.service;
 
 import java.util.List;
 
+import javax.security.auth.login.LoginException;
+
 import com.GorgeousGlam.DTO.CustomerDTO;
+import com.GorgeousGlam.exception.AddressException;
 import com.GorgeousGlam.exception.CustomerException;
+import com.GorgeousGlam.model.Address;
 import com.GorgeousGlam.model.Customer;
 
 public interface ICustomerService {
@@ -11,9 +15,15 @@ public interface ICustomerService {
 
 	public Customer getCustomerDetailsById(Integer customerId) throws CustomerException;
 
-	public Customer deleteCustomerById(Integer customerId) throws CustomerException;
+	public Customer deleteCustomerById(Integer customerId) throws CustomerException, LoginException;
 
-	public Customer updateCustomerDetails(Customer customer, Integer addressId) throws CustomerException;
+	public Customer updateCustomerDetails(CustomerDTO customer, Integer addressId, Integer customerId)
+			throws CustomerException;
 
 	public List<Customer> getAllCustomers() throws CustomerException;
+
+	public Customer addAddress(Address address, Integer customerId) throws AddressException;
+
+	public Customer deleteAddress(Integer addressId, Integer customerId) throws AddressException;
+
 }
